10535dc498163bf2060d8893870ab2597457b358
faraway10 <42tree42@gmail.com>
2024-08-25 02:14:32 +0300

    lesson 6

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -8,10 +8,15 @@ public class HelloWorld {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        app.get("/hello", context -> context.result(
-                "Hello, "
-                + context.queryParamAsClass("name", String.class).getOrDefault("World")
-                + "!"));
+
+        app.get("/users/{id}/post/{postId}", context -> {
+            var userId = context.pathParam("id");
+            var postId = context.pathParam("postId");
+            context.result(
+                    "userId: " + userId + "\n"
+                    + "postId: " + postId
+            );
+        });
 
         app.start(7070); // Стартуем веб-сервер
     }
