dc2e1793a08a7206ea7ee6869e45d72f067e4ec3
faraway10 <42tree42@gmail.com>
2024-09-11 02:02:09 +0300

    lesson 15: CRUD

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,12 +2,7 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
-import static io.javalin.rendering.template.TemplateUtil.model;
-
-import io.javalin.validation.ValidationException;
-import org.example.hexlet.dto.courses.BuildCoursePage;
-import org.example.hexlet.dto.courses.CoursePage;
-import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.controller.CoursesController;
 import org.example.hexlet.model.Course;
 import org.example.hexlet.repository.CourseRepository;
 
@@ -23,62 +18,12 @@ public class HelloWorld {
             config.fileRenderer(new JavalinJte());
         });
 
-        app.get("/", ctx -> {
-            ctx.redirect(NamedRoutes.coursesPath());
-        });
+        app.get("/", ctx -> {ctx.redirect(NamedRoutes.coursesPath());});
 
-        app.get(NamedRoutes.buildCoursePath(), ctx -> {
-            var page = new BuildCoursePage();
-            ctx.render("courses/build.jte", model("page", page));
-        });
-
-        app.post(NamedRoutes.coursesPath(), ctx -> {
-            var name = ctx.formParam("name");
-            var description = ctx.formParam("description");
-            try {
-                name = ctx.formParamAsClass("name", String.class)
-                        .check(value -> value.length() > 2, "Course name must be longer than 2 characters")
-                        .get();
-
-                description = ctx.formParamAsClass("description", String.class)
-                        .check(value -> value.length() > 10, "Course description must be longer than 10 characters")
-                        .get();
-
-                var course = new Course(name, description);
-                CourseRepository.save(course);
-                ctx.redirect(NamedRoutes.coursesPath());
-            } catch (ValidationException e) {
-                var page = new BuildCoursePage(name, description, e.getErrors());
-                ctx.render("courses/build.jte", model("page", page));
-            }
-
-        });
-
-
-        app.get(NamedRoutes.coursePath("{id}"), ctx -> {
-            var id = ctx.pathParam("id");
-            var course = CourseRepository.getEntities()
-                    .stream()
-                    .filter(c -> c.getId() == Long.parseLong(id))
-                    .findFirst()
-                    .orElseThrow(() -> new RuntimeException("Cannot find course with id = " + id)); // db fetch simulation
-            var page = new CoursePage(course);
-            ctx.render("courses/course.jte", model("page", page));
-        });
-
-        app.get(NamedRoutes.coursesPath(), ctx -> {
-            var term = ctx.queryParam("term");
-
-            var courses = CourseRepository.getEntities(); // db fetch simulation
-
-            if (term != null) {
-                courses = courses.stream().filter(course -> course.getName().toLowerCase().contains(term.toLowerCase())).toList();
-            }
-
-            var header = "Programming courses";
-            var page = new CoursesPage(courses, header);
-            ctx.render("courses/index.jte", model("page", page));
-        });
+        app.get(NamedRoutes.buildCoursePath(), CoursesController::build);
+        app.get(NamedRoutes.coursesPath(), CoursesController::index);
+        app.get(NamedRoutes.coursesPath() + "/{id}", CoursesController::show);
+        app.post(NamedRoutes.coursesPath(), CoursesController::create);
 
         app.start(7070);
     }
---
+++ src/main/java/org/example/hexlet/controller/CoursesController.java
@@ -0,0 +1,64 @@
+package org.example.hexlet.controller;
+
+import io.javalin.http.Context;
+import io.javalin.validation.ValidationException;
+import org.example.hexlet.NamedRoutes;
+import org.example.hexlet.dto.courses.BuildCoursePage;
+import org.example.hexlet.dto.courses.CoursePage;
+import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.model.Course;
+import org.example.hexlet.repository.CourseRepository;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+public class CoursesController {
+    public static void index(Context ctx) {
+        var term = ctx.queryParam("term");
+
+        var courses = CourseRepository.getEntities(); // db fetch simulation
+
+        if (term != null) {
+            courses = courses.stream().filter(course -> course.getName().toLowerCase().contains(term.toLowerCase())).toList();
+        }
+
+        var page = new CoursesPage(courses, "Programming courses");
+        ctx.render("courses/index.jte", model("page", page));
+    }
+
+    public static void show(Context ctx) {
+        var id = ctx.pathParam("id");
+        var course = CourseRepository.getEntities()
+                .stream()
+                .filter(c -> c.getId() == Long.parseLong(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Cannot find course with id = " + id)); // db fetch simulation
+        var page = new CoursePage(course);
+        ctx.render("courses/course.jte", model("page", page));
+    }
+
+    public static void build(Context ctx) {
+        var page = new BuildCoursePage();
+        ctx.render("courses/build.jte", model("page", page));
+    }
+
+    public static void create(Context ctx) {
+        var name = ctx.formParam("name");
+        var description = ctx.formParam("description");
+        try {
+            name = ctx.formParamAsClass("name", String.class)
+                    .check(value -> value.length() > 2, "Course name must be longer than 2 characters")
+                    .get();
+
+            description = ctx.formParamAsClass("description", String.class)
+                    .check(value -> value.length() > 10, "Course description must be longer than 10 characters")
+                    .get();
+
+            var course = new Course(name, description);
+            CourseRepository.save(course);
+            ctx.redirect(NamedRoutes.coursesPath());
+        } catch (ValidationException e) {
+            var page = new BuildCoursePage(name, description, e.getErrors());
+            ctx.render("courses/build.jte", model("page", page));
+        }
+    }
+}
